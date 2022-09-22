de30ff8720210e42b1e8230e4268b5fb11f4d23f
ibtihelbs <91198460+ibtihelbs@users.noreply.github.com>
2022-09-22 10:33:08 +0200

    second

--- oop.js
+++ oop.js
@@ -1,5 +1,6 @@
 const _arr = [];
-let filtered=[];
+let filteredByTags = [];
+
 main = document.querySelector("main");
 
   let _sub_filter =(tab,ht)=>{
@@ -57,27 +58,29 @@ async function Data (){
    const res = await fetch('data.json');
 
    const data = await res.json();
-   console.log(data);
 
    data.map((data)=>{
-     filtered.push(data);
+     
      main.innerHTML +=_html(data);
    });
-   console.log(main);
    let card= document.querySelectorAll(".Card");
    let tag = document.querySelectorAll(".skills button");
    let tags=document.querySelector(".tags");
-   
+    
     tag.forEach((item)=>{
       item.addEventListener("click", (event)=>{
         document.querySelector(".pop").style.display = "flex";
         let x =event.target.innerHTML;
-        console.log(x);
-        tags.innerHTML += `<button>${x}</button>`;
+        if(!filteredByTags.includes(x)){ 
+          filteredByTags.push(x) 
+          tags.innerHTML += `<button>${x}</button>`
+        }
+        else{
+          return;
+        }
         card.forEach((item)=>{
           if(item.innerHTML.includes(x)==false){
-            item.style.display = "none";
-            
+            item.style.display = "none";         
          }
         });
        
@@ -87,11 +90,11 @@ async function Data (){
       let x =event.target.innerHTML;
       let y =event.target;
        y.remove();
+      
        let _filter=tags.innerHTML.replaceAll("</button>","");
           _filter=_filter.replaceAll("<button>",",");
           _filter=_filter.split(",");
           _filter.shift();
-        console.log(_filter);
       card.forEach((item)=>{
         console.log(item.innerHTML);
       
@@ -101,9 +104,10 @@ async function Data (){
        }
       })
     })
+    console.log(filteredByTags, 'this is filtered by tag');
     document.querySelector("#clear").addEventListener("click", (event)=>{
       data.map((data)=>{
-        filtered.push(data);
+        
         main.innerHTML +=_html(data);
         document.querySelector(".pop").style.display = "none";
       });
